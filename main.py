8948d4d497f34d364d681c6a2ab7277109ba75c3
Jai Suriya <85334558+jairoyy@users.noreply.github.com>
2024-10-23 17:24:05 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,95 @@
+# name = input("Enter your Name:")
+# print( "Hello" + name)
+
+
+
+#Ask for their year of birth and compute their age.
+#Print out their age 
+
+
+# year = input("Enter your year of birth:")
+# age = input("Enter your age:")
+
+# print("Your age is " + age )
+
+
+
+# Read two numbers in (user input).
+# Print out their ratio.
+# Figure out how to make this program fail. (There are many ways.)
+
+ 
+# The user enters two numbers.
+# Store the numbers in two variables called input1 and input2.
+# Swap the values of the two variables so that input1 has the value of input2
+# and vice versa.
+# Print out the two variables.
+
+
+# a = input("Enter value a:")
+# b = input("Enter value b:")
+
+# input1 = a
+# input2 = b
+
+# c = input1
+# input1 = input2
+# input2 = c
+
+
+# print("Swaping a nd b")
+# print(input1)
+# print(input2)
+
+
+# Given a sentence, reverse each word in the sentence
+
+# name = "Python"
+# number = "22"
+
+# print("Output:" + " " + name + number)
+
+
+# msg = "Python is danger"
+
+# print(msg)
+
+# print(len(msg))
+
+# print(msg[2])
+
+# print(msg[-2])
+
+# print(msg[0:2] + msg[4:6])
+
+
+
+# for x in "apple":
+#     print(x)
+
+# for x in ["apple","mango","orange"]:
+#     for x in "apple":
+#         print(x)
+
+
+# for x in range[1,1000,1]:
+
+# name = list(range(5,1000,5))
+
+# print(name)
+
+
+
+
+
+for x in list(range(5,1000,5)):
+    print(x)
+
+
+
+
+
+
+
+
+
